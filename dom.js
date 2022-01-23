f23849489e25844f3b0470decee154a7de02d745
aryannov25 <86925205+aryannov25@users.noreply.github.com>
2022-01-24 01:21:09 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,22 @@
+
+
+
+toss = () => {
+
+    
+let toss = Math.random();
+console.log(toss)
+
+if (toss < 0.5) {
+    document.getElementById("result").innerHTML = "Heads";
+}
+
+else {
+    document.getElementById("result").innerHTML = "Tails";
+}
+   
+
+} 
+
+
+
